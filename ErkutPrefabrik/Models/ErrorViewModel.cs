bcbed560ef89e76634401fb6b8472add1d6500d7
Emirhanerbil <emirhan117net@gmail.com>
2023-01-28 04:22:44 +0300

    Add project files.

---
+++ ErkutPrefabrik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ErkutPrefabrik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
