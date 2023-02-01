34cfa69ac93a52a7cb62ad1a8e5211f232bab09e
Emirhanerbil <emirhan117net@gmail.com>
2023-02-01 05:20:24 +0300

    Contact

--- ErkutPrefabrik/Controllers/HomeController.cs
+++ ErkutPrefabrik/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using ErkutPrefabrik.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
 
 namespace ErkutPrefabrik.Controllers
 {
@@ -28,5 +30,42 @@ namespace ErkutPrefabrik.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Contact(Mail model)
+        {
+            MailMessage email = new MailMessage();
+            email.To.Add("emirhan_1095_2001@hotmail.com");
+            email.From = new MailAddress("emirhan_1095_2001@hotmail.com");
+            email.Subject = "Websitesi: " + model.Subject;
+            email.Body = "<b>İsim:</b> " + model.Name + "<br/><b>Mail:</b>  "+model.From + "<br/><b>İçerik: </b>" + model.Context + model.Phone;
+            email.IsBodyHtml = true;
+
+
+
+            SmtpClient smtp = new SmtpClient();
+
+            smtp.UseDefaultCredentials = false;
+
+            smtp.Credentials = new NetworkCredential("emirhan_1095_2001@hotmail.com", "010203040506Ee");
+            smtp.Host = "smtp.live.com";
+            smtp.EnableSsl = true;
+            smtp.Port = 465;    
+
+
+            
+
+
+            return View();
+        }
     }
+
+
+
 }
---
+++ ErkutPrefabrik/Models/Mail.cs
@@ -0,0 +1,12 @@
+﻿namespace ErkutPrefabrik.Models
+{
+    public class Mail
+    {
+        public string? Subject { get; set; }
+        public string? Name { get; set; }
+        public string? From { get; set; }
+
+        public string? Context { get; set; }
+        public string? Phone { get; set; }
+    }
+}
